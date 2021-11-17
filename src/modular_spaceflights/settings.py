c871871e607229e03401a298f1c08b987d9202f4
studioswong <studioswong@gmail.com>
2021-11-17 14:17:20 +0000

    set up session store

--- src/modular_spaceflights/settings.py
+++ src/modular_spaceflights/settings.py
@@ -7,14 +7,12 @@ HOOKS = (ProjectHooks(),)
 # List the installed plugins for which to disable auto-registry
 # DISABLE_HOOKS_FOR_PLUGINS = ("kedro-viz",)
 
-# Define where to store data from a KedroSession. Defaults to BaseSessionStore.
-# from kedro.framework.session.store import ShelveStore
-# SESSION_STORE_CLASS = ShelveStore
+from kedro_viz.integrations.kedro.sqlite_store import SQLiteStore
+from pathlib import Path
+SESSION_STORE_CLASS = SQLiteStore
 
 # Define keyword arguments to be passed to `SESSION_STORE_CLASS` constructor
-# SESSION_STORE_ARGS = {
-#     "path": "./sessions"
-# }
+SESSION_STORE_ARGS = {"path": str(Path(__file__).parents[2] / "data")}
 
 # Define custom context class. Defaults to `KedroContext`
 # CONTEXT_CLASS = KedroContext
